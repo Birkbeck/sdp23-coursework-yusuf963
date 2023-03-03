f546d7d53c21b4f129a70da0e5e9f6a17446910a
yusuf963 <mohammad963yusuf@gmail.com>
2023-03-03 17:02:56 +0000

    fix Labels class

--- src/sml/Labels.java
+++ src/sml/Labels.java
@@ -3,19 +3,27 @@ package sml;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 // TODO: write a JavaDoc for the class
+/**
+ a class for storing labels and their associated addresses as key-value pairs.
+ labels can be added to the map with their associated addresses.
+ the address associated with a label can be retrieved from the map.
+ duplicate labels are not allowed.
+ the class provides methods to check for equality with other instances, and to reset the labels.
+ */
 
 /**
  *
- * @author ...
+ * @author yusuf963
  */
 public final class Labels {
 	private final Map<String, Integer> labels = new HashMap<>();
 
 	/**
 	 * Adds a label with the associated address to the map.
-	 *
+	 * throw run time exception if label is already exists, otherwise it accepts the label
 	 * @param label the label
 	 * @param address the address the label refers to
 	 */
@@ -23,7 +31,7 @@ public final class Labels {
 		Objects.requireNonNull(label);
 		// TODO: Add a check that there are no label duplicates.
 		if(label.contains(label)){
-			throw new RuntimeException("the lable can not be added, as its already exist!");
+			throw new RuntimeException("the label can not be added, as its already exist!");
 		}else{
 			labels.put(label, address);
 		}
@@ -44,8 +52,8 @@ public final class Labels {
 	 * this issue arises when we store an address for an index that exceeds the size of the program ArrayList of Instructions in the Machine class.
 	 *  If we attempt to jump to the index of this label,and it is outside the size of program, the program will throw a NullPointerException.
 	*/
-		if(!labels.containsKey(lable)){
-			throw new RuntimeException("Lable of value "+ label+ "doesn't exist");
+		if(!labels.containsKey(label)){
+			throw new RuntimeException("Label of value "+ label+ "doesn't exist");
 		}else{
 			return labels.get(label);
 		}
@@ -60,11 +68,10 @@ public final class Labels {
 	@Override
 	public String toString() {
 		// TODO: Implement the method using the Stream API (see also class Registers).
-		return label.enterySet().stream().map(
-			e -> e.getKey() + "" + e.getValue()
-		).collect(
-			collectors.joining(", ", "[", "]")
-		);
+		return labels.entrySet()
+				.stream()
+				.map(e -> e.getKey() + " -> " + e.getValue())
+				.collect(Collectors.joining(", ", "[", "]"));
 	}
 
 	// TODO: Implement equals and hashCode (needed in class Machine).
@@ -76,6 +83,10 @@ public final class Labels {
 		return Objects.equals(labels, labels1.labels);
 	}
 
+	/**
+	 * The hash code is generated using the labels map.
+	 * @return the hash label for the object
+	 */
 	@Override
 	public int hashCode() {
 		return Objects.hash(labels);
@@ -87,4 +98,5 @@ public final class Labels {
 	public void reset() {
 		labels.clear();
 	}
+
 }
