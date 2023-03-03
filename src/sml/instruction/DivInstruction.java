b1f508e5f7dc03b1e5f6d996411bef742368c3c7
yusuf963 <mohammad963yusuf@gmail.com>
2023-03-03 12:08:51 +0000

    run formatter

--- src/sml/instruction/DivInstruction.java
+++ src/sml/instruction/DivInstruction.java
@@ -1,33 +1,37 @@
 package sml.instruction;
+
 import sml.Instruction;
 import sml.Machine;
 import sml.RegisterName;
 
 public class DivInstruction extends Instruction {
     private final RegisterName result;
-    private  final RegisterName source;
-    public  static final String OP_CODE = "div";
+    private final RegisterName source;
+    public static final String OP_CODE = "div";
+
     public DivInstruction(String label, RegisterName result, RegisterName source) {
         super(label, OP_CODE);
         this.result = result;
         this.source = source;
     }
+
     @Override
-    public int execute(Machine m){
+    public int execute(Machine m) {
         int firstValue = m.getRegisters().get(result);
         int secondValue = m.getRegisters().get(source);
-        if(secondValue == 0){
-            throw new RuntimeException(source+"is not set correctly");
+        if (secondValue == 0) {
+            throw new RuntimeException(source + "is not set correctly");
         }
         m.getRegisters().set(result, firstValue / secondValue);
         return NORMAL_PROGRAM_COUNTER_UPDATE;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
 //        return  getLabelString() + getOpcode() + " " + result + ""+ source;
         return String.format("%s%s %s %s", getLabelString(), getOpcode(), result, source);
     }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -41,10 +45,10 @@ public class DivInstruction extends Instruction {
     }
 
     @Override
-    public  int hashCode(){
-        if (label != null ){
+    public int hashCode() {
+        if (label != null) {
             return Object.hash(label, opcode, result, source);
-        }else {
+        } else {
             return Object.hash(opcode, result, source);
         }
     }
