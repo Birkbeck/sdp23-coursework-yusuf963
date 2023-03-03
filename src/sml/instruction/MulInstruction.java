69d09df3768bdf1d1b8366566adf0c10f5f553a4
yusuf963 <mohammad963yusuf@gmail.com>
2023-03-03 17:45:35 +0000

    write doc for MulInstruction run code formatter

--- src/sml/instruction/MulInstruction.java
+++ src/sml/instruction/MulInstruction.java
@@ -4,6 +4,13 @@ import sml.*;
 
 import java.util.Objects;
 
+
+/**
+ * Represents a multiplication instruction that multiplies the values in two specified registers and stores the result in the first register.
+ * Inherits from the Instruction class and implements the FlowHandling interface.
+ *
+ * @author yusuf963
+ */
 public class MulInstruction extends Instruction implements FlowHandling {
     public static final String OP_CODE = "mul";
     private final RegisterName result;
@@ -20,7 +27,7 @@ public class MulInstruction extends Instruction implements FlowHandling {
         int value1 = m.getRegisters().get(result);
         int value2 = m.getRegisters().get(source);
         int res = value1 * value2;
-        FlowHelper helper = (a, b, c) -> (a * b) != ((long) a * (long) b);
+        FlowHelper helper = (a, b, c) -> ((long) a * b) != ((long) a * (long) b);
         handleOverUnderFlow(value1, value2, res, result.toString(), source.toString(), opcode, helper);
         m.getRegisters().set(result, res);
         return NORMAL_PROGRAM_COUNTER_UPDATE;
@@ -31,6 +38,11 @@ public class MulInstruction extends Instruction implements FlowHandling {
         return getLabelString() + getOpcode() + " " + result + " " + source;
     }
 
+    /**
+     * Overrides the execute method to execute the multiplication instruction by retrieving the values from the specified
+     * registers, multiplying them, and storing the result in the first register.
+     * Handles overflow and underflow using a lambda function and the FlowHandling interface.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -43,12 +55,15 @@ public class MulInstruction extends Instruction implements FlowHandling {
         }
     }
 
+    /**
+     * Overrides the hashCode method to generate a hash code value for the MulInstruction
+     * object based on the opcode, result register, and source register.
+     * If the label is not null, it is also included in the hash code generation.
+     */
     @Override
     public int hashCode() {
         if (label != null) {
             return Objects.hash(label, opcode, result, source);
-        } else {
-            return Objects.hash(opcode, result, source);
-        }
+        } else return Objects.hash(opcode, result, source);
     }
 }
