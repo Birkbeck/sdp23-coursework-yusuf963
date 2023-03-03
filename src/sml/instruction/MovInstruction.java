ce13454e33f39c1f3bd868f89fde3a963b2b1f8d
yusuf963 <mohammad963yusuf@gmail.com>
2023-03-03 17:39:24 +0000

    write doc for MovInstruction and optimize code readability

--- src/sml/instruction/MovInstruction.java
+++ src/sml/instruction/MovInstruction.java
@@ -1,54 +1,65 @@
 package sml.instruction;
-
-import sml.Instruction;
-import sml.Machine;
+import java.util.Objects;
 import sml.RegisterName;
 import sml.Registers;
+import sml.Instruction;
+import sml.Machine;
 
-import java.util.Objects;
-
+/**
+ Represents a mov instruction that sets the value of a specified register to a given value.
+ Inherits from the Instruction class.
+ @author yusuf963
+ */
 public class MovInstruction extends Instruction {
 
     public static final String OP_CODE = "mov";
-    private final RegisterName registerToSet;
+    private final RegisterName registerName;
     private final int value;
 
-    public MovInstruction(String label, RegisterName registerToSet, int value) {
+    public MovInstruction(String label, RegisterName registerName, int value) {
         super(label, OP_CODE);
-        this.registerToSet = registerToSet;
+        this.registerName = registerName;
         this.value = value;
     }
 
     @Override
     public int execute(Machine m) {
         Registers registers = m.getRegisters();
-        registers.set(registerToSet, value);
+        registers.set(registerName, value);
         return NORMAL_PROGRAM_COUNTER_UPDATE;
     }
 
     @Override
     public String toString() {
-        return getLabelString() + getOpcode() + " " + registerToSet + " " + value;
+        return getLabelString() + getOpcode() + " " + registerName + " " + value;
     }
 
+    /**
+     Overrides the equals method to compare two MovInstructions for equality.
+     Two instructions are considered equal if they have the same label, register to set, and value.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         MovInstruction that = (MovInstruction) o;
         if (label != null) {
-            return label.equals(that.label) && value == that.value && registerToSet.equals(that.registerToSet);
+            return label.equals(that.label) && value == that.value && registerName.equals(that.registerName);
         } else {
-            return value == that.value && registerToSet.equals(that.registerToSet);
+            return value == that.value && registerName.equals(that.registerName);
         }
     }
 
+    /**
+     Overrides the hashCode method to generate a unique hash code for each MovInstruction object.
+     The hash code is based on the label, opcode, register to set, and value of the instruction.
+     */
     @Override
     public int hashCode() {
         if (label != null) {
-            return Objects.hash(label, opcode, registerToSet, value);
+            return Objects.hash(label, opcode, registerName, value);
         } else {
-            return Objects.hash(opcode, registerToSet, value);
+            return Objects.hash(opcode, registerName, value);
         }
     }
 }
